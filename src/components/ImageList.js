adfbb812471499733fc593c1dd84b07f11a90356
TSN Student <student@ulem.org>
2018-12-18 10:09:03 -0500

    Added key to each rendered img for better performance

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const ImageList = (props) => {
+    console.log(props.images);
     const images = props.images.map(img => {
-        return <img src={img.urls.thumb} />
+        return <img key={img.id} src={img.urls.thumb} />
     });
     return <div>{images}</div>
 }
