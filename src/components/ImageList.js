894fedf1f87f385f4f718efea77be812e2fe0e14
TSN Student <student@ulem.org>
2018-12-18 10:04:29 -0500

    successfully rendered images within ImageList

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const ImageList = () => {
-    
+const ImageList = (props) => {
+    const images = props.images.map(img => {
+        return <img src={img.urls.thumb} />
+    });
+    return <div>{images}</div>
 }
 
 export default ImageList;
